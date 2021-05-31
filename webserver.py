8ed362dced31752731c756507bac38387781b2bf
filipgaming008 <83877977+filipgaming008@users.noreply.github.com>
2021-06-01 00:08:31 +0200

    Create webserver.py
    webserver for uptime robot

---
+++ webserver.py
@@ -0,0 +1,19 @@
+from flask import Flask 
+from threading import Thread
+from flask import request
+from flask import jsonify
+
+app = Flask('') 
+
+@app.route('/') 
+def home():
+    return "Hello. I am alive!"
+
+
+def run():
+    app.run(host='0.0.0.0',port=8080) 
+
+
+def keep_aliveO():
+    t = Thread(target=run)
+    t.start()
