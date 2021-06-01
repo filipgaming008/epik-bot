7afdd8fc4c435f50a6dee99d8d8b3aa4a652d641
Filip <filip.dimovskifico@gmail.com>
2021-06-01 17:04:20 +0200

    PH-KDX fixed all of this, but i fixed comments cause i dont leik.

--- main.py
+++ main.py
@@ -1,79 +1,80 @@
-#imported stuff from discord, very useful for bot to work
-import discord, os
-from discord import message, Embed
+# imported stuff from discord, very useful for bot to work
+import discord
+import os
+from discord import Embed
 from discord.ext import commands
-from webserver import keep_aliveO
+from webserver import keep_alive
 
-#this took 3 hours to do
+# this took 3 hours to do
 bruh = discord.Intents.default()
 bruh.members = True
 
-#and so did this
+# and so did this
 client = commands.Bot(
-  command_prefix = "!",
-  intents = bruh,
+  command_prefix="!",
+  intents=bruh,
   help_command=None
   )
 
-#events
+# events
 
-#on ready event
+# on ready event
 @client.event
 async def on_ready():
     print("Im ready!")
 
-#join event
+# join event
 @client.event
 async def on_member_join(member):
-  for channel in member.guild.channels:
-    if str(channel) == "welcome":
-      embed = Embed(color=0xff0000)
-      embed.add_field(
-        name="Welcome!",
-        value=f"{member.mention} has joined the server!",
-        inline=False
-        )
-      embed.set_image(url=member.avatar_url)
-      
-      await channel.send(embed=embed)
+    for channel in member.guild.channels:
+        if str(channel) == "welcome":
+            embed = Embed(color=0xff0000)
+            embed.add_field(
+                name="Welcome!",
+                value=f"{member.mention} has joined the server!",
+                inline=False
+                )
+            embed.set_image(url=member.avatar_url)
+            await channel.send(embed=embed)
 
-#leave event
+# leave event
 @client.event
 async def on_member_remove(member):
-  for channel in member.guild.channels:
-    if str(channel) == "welcome":
-      embed = Embed(color=0xff0000)
-      embed.add_field(
-        name="Good bye!",
-        value=f"{member.mention} has left the server!",
-        inline=False
-        )
-      embed.set_image(url=member.avatar_url)
-      
-      await channel.send(embed=embed)
+    for channel in member.guild.channels:
+        if str(channel) == "welcome":
+            embed = Embed(color=0xff0000)
+            embed.add_field(
+                name="Good bye!",
+                value=f"{member.mention} has left the server!",
+                inline=False
+                )
+            embed.set_image(url=member.avatar_url)
+            await channel.send(embed=embed)
 
-#commands
+# commands
 
 @client.command()
 async def hello(ctx):
-  await ctx.send("Hello!")
+    await ctx.send("Hello!")
 
 @client.command()
 async def ping(ctx):
-  await ctx.send(f"Pong! {round(client.latency, 3)* 100}ms")
+    await ctx.send(f"Pong! {round(client.latency, 3)* 100}ms")
 
-#help command
+# help command
 @client.command()
 async def help(ctx):
-  embed=Embed(
-    title = "\nAvailable commands:\n", 
-    description = "", 
-    color = 0xff0000)
-  embed.add_field(
-    name = "\nBasic commands:\n", 
-    value = "!help - brings up this menu \n!ping - pings the bot \n!hello - say hi to the bot", 
-    inline = False)
-  await ctx.send(embed=embed)
+    embed = Embed(
+        title="\nAvailable commands:\n",
+        description="",
+        color=0xff0000
+        )
+    embed.add_field(
+        name="\nBasic commands:\n",
+        value="!help - brings up this menu \n!ping - pings the bot \n!hello - say hi to the bot",
+        inline=False
+        )
+    await ctx.send(embed=embed)
 
-keep_aliveO()
+keep_alive()
 client.run(os.getenv("TOKEN"))
