65167792c31ce54b163b2b0a5fdb3751be751d49
Filip <filip.dimovskifico@gmail.com>
2021-06-02 16:51:36 +0200

    fixed some perms with load and unload cogs

--- botcode.py
+++ botcode.py
@@ -19,15 +19,30 @@ client=commands.Bot(
 # commands
 
 # load bot_cogs
-@client.command()
+@client.command(pass_context=True)
+@has_permissions(administrator=True)
 async def load(ctx, extension):
     client.load_extension(f"bot_cogs.{extension}")
 
+# load bot_cogs error handle
+@load.error
+async def load_error(ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("You cant do that!")
+
 # unload bot_cogs
-@client.command()
+@client.command(pass_context=True)
+@has_permissions(administrator=True)
 async def unload(ctx, extension):
     client.unload_extension(f"bot_cogs.{extension}")
 
+# unload bot_cogs error handle
+@unload.error
+async def unload_error(ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("You cant do that!")
+
+# some code to check what files are there in bot_cogs
 for filename in os.listdir("./bot_cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"bot_cogs.{filename[:-3]}")
