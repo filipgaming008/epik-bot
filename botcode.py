9877f39ccecf8d956e855147bb0e45261f467e64
Filip <filip.dimovskifico@gmail.com>
2021-06-02 13:40:55 +0200

    made a better ping command and fixed the purge for invalid amount

--- botcode.py
+++ botcode.py
@@ -7,10 +7,10 @@ from webserver import keep_alive
 
 # this took 3 hours to do
 bruh = discord.Intents.default()
-bruh.members = True
+bruh.members=True
 
 # and so did this
-client = commands.Bot(
+client=commands.Bot(
     command_prefix="!",
     intents=bruh,
     help_command=None
@@ -34,8 +34,8 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     for channel in member.guild.channels:
-        if str(channel) == "welcome":
-            embed = Embed(color=0xff0000)
+        if str(channel)=="welcome":
+            embed=Embed(color=0xff0000)
             embed.add_field(
                 name="Welcome!",
                 value=f"{member.mention} has joined the server!",
@@ -48,8 +48,8 @@ async def on_member_join(member):
 @client.event
 async def on_member_remove(member):
     for channel in member.guild.channels:
-        if str(channel) == "welcome":
-            embed = Embed(color=0xff0000)
+        if str(channel)=="welcome":
+            embed=Embed(color=0xff0000)
             embed.add_field(
                 name="Good bye!",
                 value=f"{member.mention} has left the server!",
@@ -68,12 +68,18 @@ async def hello(ctx):
 # ping command
 @client.command()
 async def ping(ctx):
-    await ctx.send(f"Pong! {round(client.latency, 3)* 100}ms")
+    start_time=time.time()
+    message=await ctx.send("Pinging the bot...")
+    end_time=time.time()
+    await message.edit(
+        f"Pong!\n {round(client.latency, 3)* 100}ms" 
+        f"\nAPI: {round((end_time - start_time), 3)* 100}ms"
+        )
 
 # say command
 @client.command()
 async def say(ctx, *, text):
-    message = ctx.message
+    message=ctx.message
     await message.delete()
     await ctx.send(f"{text}")
 
@@ -81,8 +87,11 @@ async def say(ctx, *, text):
 @client.command(pass_context=True)
 @has_permissions(administrator=True)
 async def purge(ctx, amount=0):
-    await ctx.channel.purge(limit=amount + 1)
-    await ctx.send(f"{amount} messages have been deleted successfully!")
+    if amount != 0:
+        await ctx.channel.purge(limit=amount + 1)
+        await ctx.send(f"{amount} messages have been deleted successfully!")
+    else:
+        await ctx.send("Invalid amount!")
 
 # purge error handle
 @purge.error
@@ -93,7 +102,7 @@ async def purge_error(ctx, error):
 # help command
 @client.command()
 async def help(ctx):
-    embed = Embed(
+    embed=Embed(
         title="\nAvailable commands:\n",
         description="",
         color=0xff0000
