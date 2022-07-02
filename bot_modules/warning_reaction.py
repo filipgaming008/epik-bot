c69dc4e87b12e169ef78ae14c2bef50e3af5a61f
Filip <filip.dimovskifico@gmail.com>
2022-07-02 23:52:01 +0200

    plz

--- bot_modules/warning_reaction.py
+++ bot_modules/warning_reaction.py
@@ -15,7 +15,7 @@ class warning_reaction(commands.Cog):
         guild = self.bot.get_guild(845624418650161172)
         print("Im on it!")
         for Member in guild.members:
-            memberid = guild.user_id
+            memberid = guild.member_id
             print(memberid)
 
 
