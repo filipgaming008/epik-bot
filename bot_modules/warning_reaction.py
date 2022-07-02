a6201f5531662c4b7dcdafe3b86477ce15c8a600
Filip <filip.dimovskifico@gmail.com>
2022-07-02 23:33:10 +0200

    eeeeeeeeeeeeeeeeee

---
+++ bot_modules/warning_reaction.py
@@ -0,0 +1,25 @@
+# imports
+import discord, time
+from discord import Embed
+from discord.ext import commands
+
+
+class warning_reaction(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Message sending
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        guild = self.bot.get_guild(845624418650161172)
+        print("Im on it!")
+        for Member in guild.members:
+            memberid = self.bot.user.id
+            print(memberid)
+
+
+
+
+def setup(bot):
+    bot.add_cog(warning_reaction(bot))
