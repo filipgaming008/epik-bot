ad38d40534b21eac5147d798d26bcab1b9857a53
Filip <filip.dimovskifico@gmail.com>
2022-07-03 00:07:09 +0200

    istg if this works

--- bot_modules/warning_reaction.py
+++ bot_modules/warning_reaction.py
@@ -15,7 +15,7 @@ class warning_reaction(commands.Cog):
         guild = self.bot.get_guild(845624418650161172)
         print("Im on it!")
         for Member in guild.members:
-            memberid = self.bot.fetch_user.id
+            memberid = Member.id
             print(memberid)
 
 
